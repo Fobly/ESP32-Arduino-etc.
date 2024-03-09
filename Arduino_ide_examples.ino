1f5427ae6bf830e788f42e9375bb86f720091abb
Fobly <160914246+Fobly@users.noreply.github.com>
2024-03-09 14:36:43 +0500

    Added PID regulator
    - PID regulator

---
+++ Arduino_ide_examples.ino
@@ -0,0 +1,10 @@
+
+// ================PID regulator================
+int computePID(float input, float setpoint, float kp, float ki, float kd, float dt) {
+  float err = setpoint - input;
+  static float integral = 0, prevErr = 0;
+  integral += err * dt;
+  float D = (err - prevErr) / dt;
+  prevErr = err;
+  return (err * kp + integral * ki + D * kd);
+}
