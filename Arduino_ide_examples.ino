a7a3b592eaa030ffe2be38eac9ef584d575be0e5
Fobly <160914246+Fobly@users.noreply.github.com>
2024-03-09 14:44:30 +0500

    Added WiFi and UDP connection
    - PID Regulator
    - WiFi + UDP Connection

--- Arduino_ide_examples.ino
+++ Arduino_ide_examples.ino
@@ -8,3 +8,27 @@ int computePID(float input, float setpoint, float kp, float ki, float kd, float 
   prevErr = err;
   return (err * kp + integral * ki + D * kd);
 }
+
+// ================WiFi Connection================
+const char ssid[] = "Testy";
+const char pass[] = "22222222";
+
+const char addr[] = "server.tasty"; //Delete if without UDP
+int port = 7001; //Delete if without UDP
+
+WiFiUDP udp; //Delete if without UDP
+
+void setup() {
+Serial.println("WIFI_CONNECTION");
+   WiFi.mode(WIFI_STA);
+   WiFi.begin(ssid, pass);
+   if (WiFi.waitForConnectResult() != WL_CONNECTED){
+      Serial.println("Fail");
+      delay(1000);
+      ESP.restart();
+   }
+   Serial.printf("Success");
+   udp.begin(WiFi.localIP(), 7000); //Delete if without UDP
+
+  
+}
